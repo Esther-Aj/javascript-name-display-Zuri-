0b2938d0c3037d09f1106c86f99b878ad3a26795
Esther Aj <chinnyestie@gmail.com>
2022-06-22 22:08:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let fname ="Esther Ajuzieogu \n";
+let height="1.75m \n";
+let country="Nigeria";
+
+let fullName= "Name: "+ fname;
+let newHeight= "Height: "+ height;
+let countryNig = "Country: "+ country;
+
+
+console.log(fullName+ newHeight+ countryNig);
+alert(fullName+ newHeight+ countryNig);
+
+
+
